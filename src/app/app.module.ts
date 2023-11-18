f7437a30d20a78dfd3a2332b808e2b5fd0d031c5
Miguel <miguelangelmolinaforero@gmail.com>
2023-11-17 23:42:47 -0500

    Vista login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { DescuentosComponent } from './components/descuentos/descuentos.component';
+import { DevengosComponent } from './components/devengos/devengos.component';
+import { EstadisticasComponent } from './components/estadisticas/estadisticas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmpleadosComponent,
+    DescuentosComponent,
+    DevengosComponent,
+    EstadisticasComponent
   ],
   imports: [
     BrowserModule,
